0cbb3665c8c44f30b100ab4a376b2a01d126949e
Ramya.U <ramyayadav2016@gmail.com>
2020-11-30 16:22:22 +0530

    [Ramya] Add . Added method to delete existing data from datatable

---
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    class AddressBookManagement
+    {
+        /// <summary>
+        /// Deletings the contact from table UC5
+        /// </summary>
+        /// <param name="datatable">The datatable.</param>
+        /// <returns></returns>
+        public DataTable DeletingContactFromTable(DataTable datatable)
+        {
+            //getting all the data except the data to be deleted
+            //saving them in new data table by copytodatatable method
+            //returning the new data table
+            DataTable dataTableupdated = datatable.AsEnumerable().Except(datatable.AsEnumerable().Where(r => r.Field<string>("firstName") == "Khloe" && r.Field<string>("lastName") == "Lamar")).CopyToDataTable();
+            foreach (var data in dataTableupdated.AsEnumerable())
+            {
+                Console.WriteLine("FirstName:- " + data.Field<string>("firstName"));
+                Console.WriteLine("lastName:- " + data.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + data.Field<string>("address"));
+                Console.WriteLine("City:- " + data.Field<string>("city"));
+                Console.WriteLine("State:- " + data.Field<string>("state"));
+                Console.WriteLine("zip:- " + Convert.ToInt32(data.Field<int>("zip")));
+                Console.WriteLine("phoneNumber:- " + Convert.ToDouble(data.Field<Double>("phoneNumber")));
+                Console.WriteLine("eMail:- " + data.Field<string>("eMail"));
+                Console.WriteLine("***************");
+            }
+            return dataTableupdated;
+        }
+    }
+}
+    
+
