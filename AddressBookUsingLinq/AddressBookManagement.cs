16e626e9cce35be5f60631897c7540c3a9941782
Ramya.U <ramyayadav2016@gmail.com>
2020-11-30 16:55:33 +0530

    [Ramya] Add . Added method to get count by type

---
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    class AddressBookManagement
+    {
+
+        /// <summary>
+        /// Gets the count of each type after grouping by type UC10
+        /// </summary>
+        /// <param name="dataTable">The data table.</param>
+        public void GetCountByType(DataTable dataTable)
+        {
+            var recordData = dataTable.AsEnumerable().GroupBy(r => r.Field<string>("type")).Select(r => new { type = r.Key, count = r.Count() });
+            foreach (var data in recordData)
+            {
+                Console.WriteLine("Count for type- " + data.count);
+            }
+        }
+    }
+}
+ 
