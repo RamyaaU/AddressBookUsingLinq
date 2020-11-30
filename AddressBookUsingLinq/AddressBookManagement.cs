dce43d2c73a6aadbedf8e226fd862614e64b736a
Ramya.U <ramyayadav2016@gmail.com>
2020-11-30 16:31:19 +0530

    [Ramya] Add . Addded method to retrieve data by city or state

---
+++ AddressBookUsingLinq/AddressBookManagement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    class AddressBookManagement
+    {
+        /// <summary>
+        /// Retrievings the contact details by state or city. UC6
+        /// </summary>
+        /// <param name="dataTable">The data table.</param>
+        public void RetrievingContactDetailsByStateOrCity(DataTable dataTable)
+        {
+            //lambda syntax for getting data for particular city
+            var recordData = dataTable.AsEnumerable().Where(r => r.Field<string>("city") == "Celtics");
+            //lambda syntax for getting data for particular state
+            var recordDataState = dataTable.AsEnumerable().Where(r => r.Field<string>("state") == "Charlotte");
+            foreach (var data in recordDataState)
+            {
+                Console.WriteLine("FirstName:- " + data.Field<string>("firstName"));
+                Console.WriteLine("lastName:- " + data.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + data.Field<string>("address"));
+                Console.WriteLine("City:- " + data.Field<string>("city"));
+                Console.WriteLine("State:- " + data.Field<string>("state"));
+                Console.WriteLine("zip:- " + Convert.ToInt32(data.Field<int>("zip")));
+                Console.WriteLine("phoneNumber:- " + Convert.ToDouble(data.Field<Double>("phoneNumber")));
+                Console.WriteLine("eMail:- " + data.Field<string>("eMail"));
+                Console.WriteLine("***************");
+            }
+        }
+    }
+}
+   
