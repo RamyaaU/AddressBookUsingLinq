d606d1f00e863a7ce0f328b5acb3e5f42f2030f6
Ramya.U <ramyayadav2016@gmail.com>
2020-11-30 15:59:53 +0530

    [Ramya] Add . Added method to add columns into datatable

--- AddressBookUsingLinq/Program.cs
+++ AddressBookUsingLinq/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace AddressBookUsingLinq
 {
@@ -11,6 +12,60 @@ namespace AddressBookUsingLinq
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Welcome to Address Book Using Linq!");
+            //Creating DataTable for addressbook problem UC1
+            DataTable addressBookTable = new DataTable();
+
+            //adding columns into address book table UC2
+            DataColumn column;
+            DataRow row;
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "firstName";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "lastName";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "address";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "city";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "state";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "zip";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Double");
+            column.ColumnName = "phoneNumber";
+            addressBookTable.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "eMail";
+            addressBookTable.Columns.Add(column);
+
+            // Make the first name and mobile no column the primary key column
+            DataColumn[] PrimaryKeyColumns = new DataColumn[2];
+            PrimaryKeyColumns[0] = addressBookTable.Columns["firstName"];
+            PrimaryKeyColumns[1] = addressBookTable.Columns["phoneNumber"];
+            addressBookTable.PrimaryKey = PrimaryKeyColumns;
         }
     }
 }
+
+    
